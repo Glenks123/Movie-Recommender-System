39aa41c29bb762e012126e010a6c044bf29676b3
Glenks123 <63196484+Glenks123@users.noreply.github.com>
2022-04-05 16:20:15 +0300

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,30 @@
+import numpy as np
+import sys
+from os.path import join
+from matplotlib import pyplot
+
+sys.path.append('..')
+
+
+def normalizeRatings(Y, R):
+    m, n = Y.shape
+    Ymean = np.zeros(m)
+    Ynorm = np.zeros(Y.shape)
+
+    for i in range(m):
+        idx = R[i, :] == 1
+        Ymean[i] = np.mean(Y[i, idx])
+        Ynorm[i, idx] = Y[i, idx] - Ymean[i]
+
+    return Ynorm, Ymean
+
+
+def loadMovieList():
+    with open(join('data', 'movie_ids.txt'), encoding='ISO-8859-1') as fid:
+        movies = fid.readlines()
+
+    movieNames = []
+    for movie in movies:
+        parts = movie.split()
+        movieNames.append(' '.join(parts[1:]).strip())
+    return movieNames
